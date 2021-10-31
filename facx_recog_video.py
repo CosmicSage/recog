744d7abe9e703612e3ca8949422ce1d3149d4b29
CosmicSage <prabhakarmishra9@yahoo.com>
2021-10-31 15:32:10 +0530

    rm Democrat coq sucks

--- facx_recog_video.py
+++ facx_recog_video.py
@@ -19,16 +19,16 @@ obama_image = face_recognition.load_image_file("imgs/obama.jpg")
 obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
 
 # Load a second sample picture and learn how to recognize it.
-# biden_image = face_recognition.load_image_file("imgs/boy.jpg")
-# biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
+# lidaren_image = face_recognition.load_image_file("imgs/boy.jpg")
+# lidaren_face_encoding = face_recognition.face_encodings(lidaren_image)[0]
 
 # Create arrays of known face encodings and their names
 known_face_encodings = [
     obama_face_encoding,
-    # biden_face_encoding
+    # lidaren_face_encoding
 ]
 known_face_names = [
-    "Obamaa",
+    "Obamiee nigga",
     # "Akbar harp"
 ]
 
