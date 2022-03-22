49b4a4d4824cea60573578f8dd1334d81d1ec51c
CosmicSage <prabhakarmishra9@yahoo.com>
2022-03-22 23:24:57 +0530

    painful dumb sql

--- presence.sql
+++ presence.sql
@@ -1,5 +1,7 @@
 CREATE TABLE presence (
-    id INT NOT NULL PRIMARY KEY,
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
     time_spent TEXT
-)
+);
+
+INSERT INTO presence (id, name, time_spent) VALUES(abs(random()), 'Obama', '10s');
